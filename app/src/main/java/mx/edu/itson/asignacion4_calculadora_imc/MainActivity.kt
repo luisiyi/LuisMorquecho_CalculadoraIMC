05cf1c632514fe07cd2fc16e562e2a59056b6429
Luis Morquecho <luisiyi1997@gmail.com>
2021-02-15 17:43:35 -0700

    Calculadora IMC ya arranca

--- app/src/main/java/mx/edu/itson/asignacion4_calculadora_imc/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/asignacion4_calculadora_imc/MainActivity.kt
@@ -3,21 +3,31 @@ package mx.edu.itson.asignacion4_calculadora_imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var ET_Peso =0
-    var ET_Altura =0
+    var estatura:Double =0.0
+    var peso:Double =0.0
     var imc=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        /*
         var ET_Altura: EditText = findViewById(R.id.ET_Altura)
         var ET_Peso: EditText = findViewById(R.id.ET_Peso)
+        */
+
+        this.btnCalcular.setOnClickListener(){
+            TV_Resultado.setText(calculaIMC().toString())
+        }
     }
 
-    fun evaluarIMC(){
+    fun calculaIMC():Double {
+        estatura=(this.ET_Altura.text.toString()).toDouble()
+        peso=(this.ET_Peso.text.toString()).toDouble()
+        return peso/(estatura*estatura);
+
     }
 
 }
